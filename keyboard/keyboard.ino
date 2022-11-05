16265ea01d208611df711f536b41273951baa8d3
Antonio Astorino <antonio.astorino.phd@gmail.com>
2022-11-05 20:32:52 +0100

    Add LED driving

--- keyboard/keyboard.ino
+++ keyboard/keyboard.ino
@@ -1,5 +1,7 @@
 #include <Keyboard.h>
 #define LED 17
+#define LOCK_1 7
+#define LOCK_2 6
 #define CLK 21
 #define S_DATA 5
 #define COLS 16
@@ -29,20 +31,20 @@
 #define KEY_NUM 255
 
 uint8_t keys[ROWS][COLS] = {
-    {'t', 0, 0, 0, 0, 0, 0, 0, 0, 'u', 'i', 'o', 0, 0, 0, 0},
+    {'t', KEY_ESC, 0, 0, 0, 0, 0, 0, 0, 'u', 'i', 'o', 0, 0, 0, 0},
     {0, 0, 0, 0, 0, 0, 0, 0, 0, 'j', 'k', 'l', 0, 0, KEY_RETURN, 0},
     {0, 0, KEY_LEFT_SHIFT, 0, 0, 0, 0, 0, 0, 'm', ',', '.', 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, KEY_NUM, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, KEY_NUM, KEY_BACKSPACE, ' ', 0, 0, 0, 0, 0, 0, 0},
 };
 uint8_t num_keys[ROWS][COLS] = {
-    {'n', 0, 0, 0, 0, 0, 0, 0, 0, KEY_KP_7, KEY_KP_8, KEY_KP_9, KEY_KP_EQUAL,
-     '(', ')', 0},
+    {'n', KEY_ESC, 0, 0, 0, 0, 0, 0, 0, KEY_KP_7, KEY_KP_8, KEY_KP_9,
+     KEY_KP_EQUAL, '(', ')', 0},
     {0, 0, 0, 0, 0, 0, 0, 0, 0, KEY_KP_4, KEY_KP_5, KEY_KP_6, KEY_KP_PLUS,
      KEY_KP_SLASH, KEY_KP_ENTER, 0},
     {0, 0, 0, 0, 0, 0, 0, 0, 0, KEY_KP_1, KEY_KP_2, KEY_KP_3, KEY_KP_MINUS, 0,
      0, 0},
-    {0, 0, 0, 0, 0, 0, KEY_NUM, 0, 0, KEY_KP_0, 0, KEY_KP_DOT, KEY_KP_ASTERISK,
-     0, 0},
+    {0, 0, 0, 0, 0, 0, KEY_NUM, KEY_BACKSPACE, ' ', KEY_KP_0, 0, KEY_KP_DOT,
+     KEY_KP_ASTERISK, 0, 0},
 };
 uint8_t row_to_pin[ROWS] = {8, 9, 16, 10};
 char monitor_str[64] = {0};
@@ -64,13 +66,14 @@ void setup() {
                  LOW); // should disable internal pull-up resistors
   }
   pinMode(LED, OUTPUT);
+  pinMode(LOCK_1, OUTPUT);
+  pinMode(LOCK_2, OUTPUT);
   pinMode(CLK, OUTPUT);
   pinMode(S_DATA, OUTPUT);
   Keyboard.begin();
 }
 
 void loop() {
-  uint8_t ledStatus = 0;
   uint8_t button;
   keyPressed key_pressed[MAX_SIMULTANELUS_KEYS];
   uint8_t key_pressed_counter = 0;
@@ -97,28 +100,33 @@ void loop() {
         key_pressed[key_pressed_counter].col = col;
         key_pressed[key_pressed_counter].row = row;
         key_pressed_counter++;
-        if (ledStatus) {
-          digitalWrite(LED, HIGH);
-        } else {
-          digitalWrite(LED, LOW);
-        }
       } else {
         Keyboard.release(button);
       }
     }
   }
   // Change layout and do not send this button
-  current_layout = new_key_num_pressed ? &num_keys : &keys;
+  if (new_key_num_pressed) {
+    current_layout = &num_keys;
+    digitalWrite(LED, LOW);
+    digitalWrite(LOCK_1, HIGH);
+    digitalWrite(LOCK_2, HIGH);
+  } else {
+    digitalWrite(LED, HIGH);
+    digitalWrite(LOCK_1, LOW);
+    digitalWrite(LOCK_2, LOW);
+    current_layout = &keys;
+  }
   if (key_num_pressed != new_key_num_pressed) {
-    Serial.println("Rel");
+    //    Serial.println("Rel");
     Keyboard.releaseAll();
   }
   for (uint8_t p = 0; p < key_pressed_counter; p++) {
     uint8_t row = key_pressed[p].row;
     uint8_t col = key_pressed[p].col;
     uint8_t button = (*current_layout)[row][col];
-    sprintf(monitor_str, "Row: %d, Col: %d, B%d\n", row, col, button);
-    Serial.write(monitor_str);
+    //    sprintf(monitor_str, "Row: %d, Col: %d, B%d\n", row, col, button);
+    //    Serial.write(monitor_str);
     Keyboard.press(button);
   }
   key_num_pressed = new_key_num_pressed;
