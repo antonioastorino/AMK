c05631faaf68a46aefc744f9137864ce8d81b131
Antonio Astorino <antonio.astorino.phd@gmail.com>
2022-11-12 22:38:55 +0100

    Size is an input

--- tools/imager.c
+++ tools/imager.c
@@ -7,8 +7,8 @@
 #include <sys/uio.h>
 #include <unistd.h>
 
-#define MAX_IN_COLS (4)
-#define MAX_IN_ROWS (8)
+#define MAX_IN_COLS (128)
+#define MAX_IN_ROWS (32)
 #define MAX_IN_SIZE                                                                                \
     (MAX_IN_ROWS * (MAX_IN_COLS + 1)) /* The newline character requires an extra col */
 #define MAX_OUT_COLS MAX_IN_COLS
@@ -22,12 +22,16 @@ int main(int argc, char* argv[])
     uint8_t out_image[MAX_OUT_COLS][MAX_OUT_PAGES] = {0};
     char buff[MAX_IN_SIZE]                         = {0};
     size_t counter                                 = 0;
+    int rows                                       = 0;
+    int cols                                       = 0;
 
-    if (argc < 2)
+    if (argc < 4)
     {
-        printf("File not specified");
+        printf("Missing args");
         return 1;
     }
+    rows = atoi(argv[2]);
+    cols = atoi(argv[3]);
 
     int fd = open(argv[1], O_RDONLY);
     if (fd < 0)
@@ -43,15 +47,15 @@ int main(int argc, char* argv[])
     }
     printf("Read: %zd bytes\n", bytes_read);
 
-    for (size_t b = 0; b < bytes_read; b++)
+    for (size_t b = 0; b < bytes_read && row < rows; b++)
     {
         if (buff[b] == '\n')
         {
             // End of line reached
+            row++;
             continue;
         }
-        row  = counter / MAX_IN_COLS;
-        col  = counter % MAX_IN_COLS;
+        col  = counter % cols;
         page = row / 8;
         out_image[col][page] |= (buff[b] == '0') << row;
         printf("Page: %zu Col %zu Val %d\n", page, col, buff[b] == '0');
@@ -59,10 +63,10 @@ int main(int argc, char* argv[])
     }
 
     printf("{\n");
-    for (size_t p = 0; p < MAX_OUT_PAGES; p++)
+    for (size_t p = 0; p < rows / 8; p++)
     {
         printf("{");
-        for (size_t c = 0; c < MAX_OUT_COLS; c++)
+        for (size_t c = 0; c < cols; c++)
         {
             printf("0x%02x, ", out_image[c][p]);
         }
